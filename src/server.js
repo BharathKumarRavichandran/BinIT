5936d0d09bf06486eb7a49bfc880820ee85a3935
Bharath Kumar Ravichandran <bharathkumarravichandran@gmail.com>
2019-10-02 12:10:05 +0530

    Feat: Accept text/* Content-type reqs for SIM808 interfacing

--- src/server.js
+++ src/server.js
@@ -132,6 +132,16 @@ app.get('/', (req, res) => {
 });
 
 
+app.use(function(req, res, next){
+	if (req.is('text/*')) {
+	  req.text = '';
+	  req.setEncoding('utf8');
+	  req.on('data', function(chunk){ req.text += chunk });
+	  req.on('end', next);
+	} else {
+	  next();
+	}
+});
 
 // Emit data to UI when this API route is hit
 app.post('/updateBinWeight', async (req,res) => {
